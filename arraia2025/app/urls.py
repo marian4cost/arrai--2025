0c1a5a48ff04d02f918ed9d6acaef9cba31a42f8
marian4cost4 <mariana.costa.lisboa5579@gmail.com>
2025-06-06 14:53:07 -0300

    página de votação para rei rj-45

--- arraia2025/app/urls.py
+++ arraia2025/app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("candidate01", views.candidate01, name="candidate01")
 ]
--- arraia2025/app/views.py
+++ arraia2025/app/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "home.html")
+
+def candidate01(request):
+    return render(request, "candidate01.html")
